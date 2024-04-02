b5ca40bac647093e4d06174faf6074ed6275327a
Dave Cheng <dave.cheng@ycdsb.ca>
2024-04-02 09:51:25 -0400

    Final version

--- patterns.py
+++ patterns.py
@@ -44,19 +44,22 @@ while running:
         pygame.draw.line(screen, BLACK, (0, y), (400, y))
 
     # Quadrant 2
+    # Iterate over horizontal range and vertical ranges with 80 spacing
     for x in range(480, 800, 80):
         for y in range(80, 400, 80):
             pygame.draw.circle(screen, MAGENTA, (x, y), 20)
 
     # Quadrant 3
     for x in range(0, 400, 1):
-        # Calculate the color based on the position
+        # Calculate the color (0-255) based on the position (0-400)
         gradient_colour = (x / 400 * 255, x / 400 * 255, x / 400 * 255)
         pygame.draw.line(screen, gradient_colour, (x, 400), (x, 800))
 
     # Quadrant 4
+    # Iterate over horizontal range and vertical ranges 1 px at a time
     for x in range(401, 801, 1):
         for y in range(401, 801, 1):
+            # Generate new random RGB colour each loop
             color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
             pygame.draw.rect(screen, color, (x, y, 1, 1))  # Draw a 1x1 rectangle (pixel) with the random color
     # Draw initials "dc"
